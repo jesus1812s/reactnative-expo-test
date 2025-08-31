7594aeb6e4e9098275687d9be5b61cb109d2b280
Jesús Enrique Salas Gil <36157513+jesus1812s@users.noreply.github.com>
2025-08-31 19:31:13 -0400

    wip: fix styles adding tracking wider and fixes to match screens

--- components/Onboarding/components/OnboardingScreen.tsx
+++ components/Onboarding/components/OnboardingScreen.tsx
@@ -27,9 +27,9 @@ export const OnboardingScreen = ({ path, title, description, imageSource }: Onbo
 
 const styles = {
   container: `max-w-4/5`,
-  imageContainer: `items-center`,
-  image: `w-4/5 h-auto`,
+  imageContainer: `items-center mb-4`,
+  image: `w-auto h-auto`,
   getStartedContainer: `items-center mx-8 mt-4 max-w-4/5`,
-  getStartedText: `font-inter text-4xl font-extrabold text-text-primary leading-12 text-center mb-4 max-w-72`,
-  getStartedTextDescription: `font-inter text-lg font-medium text-text-secondary leading-[24px] text-center px-4`,
+  getStartedText: `font-inter text-4xl font-extrabold text-text-primary leading-12 text-center mb-4 max-w-80 tracking-wider`,
+  getStartedTextDescription: `font-inter text-lg font-medium text-text-secondary leading-12 text-center px-4`,
 };
